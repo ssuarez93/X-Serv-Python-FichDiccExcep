1574a4ca002225cae3e9ee9b3f1968a18be98950
Sara Suarez Sanchez <ssuarez@zeta40.aulas.gsyc.es>
2016-02-09 09:00:25 +0100

    Entrega ejercicio 13.5 Sara Suárez

--- ejercicio.py
+++ ejercicio.py
@@ -1,13 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-# hola, hola
+# Sistemas Teleco. Sara Suárez.
 
 fd = open('/etc/passwd', 'r')
-
 lineas = fd.readlines()
+
+diccionario_users = {}
+for linea in lineas:
+	elementos = linea.split(':')
+	user = elementos[0]
+	shell = elementos[-1]
+        diccionario_users[user]=[shell]
+
+print "Usuario root:", diccionario_users.get("root")  #De esta forma no salta una excepción
+
+try:
+    diccionario_users['imaginario']   #Uso esta forma para que me salte una excepción y manejarla
+except KeyError:
+    print "Error: Este usuario no existe"
+    
+num_users = len(lineas)
+print "Hay", num_users, "usuarios"
+
 fd.close()
 
-for linea in lineas:
-    elementos = linea.split(':')
-    print elementos[0], elementos[-1][:-1]
+
